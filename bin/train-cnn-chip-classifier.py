10ca177d122035df7a95b7b48d2e58fcc328bd5b
Nikki Aldeborgh <haaldeborgh@gmail.com>
2017-01-12 14:39:34 -0700

    first commit

---
+++ bin/train-cnn-chip-classifier.py
@@ -0,0 +1,360 @@
+# gbdx.Task(train-cnn-chip-classifier, chip_dir, min_side_dim, max_side_dim, resize_dim, hyperparams)
+
+# For now we use the geojson that is present in the output chip directory. User does not input the geojson with class names to the train task (classes are defined when chips are generated).
+
+import logging
+import ast, subprocess
+import shutil, os, sys
+import geojson, json
+import numpy as np
+
+from sklearn.metrics import classification_report
+from multiprocessing import Pool, cpu_count
+from mltools import geojson_tools as gt
+from functools import partial
+from osgeo import gdal
+from scipy.misc import imresize
+from pool_net import PoolNet
+from gbdx_task_interface import GbdxTaskInterface
+from keras.optimizers import SGD
+from keras.callbacks import ModelCheckpoint
+
+
+# log file for debugging
+logging.basicConfig(filename='out.log',level=logging.DEBUG)
+
+
+class TrainCnnChipClassifier(GbdxTaskInterface):
+
+    def __init__(self):
+
+        GbdxTaskInterface.__init__(self)
+        # self.check_chip = check_chip
+
+        # Get input string ports
+        self.max_side_dim = int(self.get_input_string_port('max_side_dim', default='150'))
+        self.min_side_dim = int(self.get_input_string_port('min_side_dim', default='0'))
+        self.classes = [i.strip() for i in self.get_input_string_port('classes').split(',')]
+        self.resize_dim = ast.literal_eval(self.get_input_string_port('resize_dim', default='None'))
+        self.two_rounds = ast.literal_eval(self.get_input_string_port('two_rounds', default='False'))
+        self.train_size = int(self.get_input_string_port('train_size', default='10000'))
+        self.train_size_2 = int(self.get_input_string_port('train_size_2', default=int(0.5 * self.train_size)))
+        self.batch_size = int(self.get_input_string_port('batch_size', default='32'))
+        self.nb_epoch = int(self.get_input_string_port('nb_epoch', default='35'))
+        self.nb_epoch_2 = int(self.get_input_string_port('nb_epoch_2', default='8'))
+        self.use_lowest_val_loss = ast.literal_eval(self.get_input_string_port('use_lowest_val_loss', default='True'))
+        self.test = ast.literal_eval(self.get_input_string_port('test', default='True'))
+        self.test_size = int(self.get_input_string_port('test_size', default='1000'))
+        self.lr_1 = float(self.get_input_string_port('learning_rate', default='0.001'))
+        self.lr_2 = float(self.get_input_string_port('learning_rate_2', default='0.01'))
+        self.bit_depth = int(self.get_input_string_port('bit_depth', default='8'))
+        self.kernel_size = int(self.get_input_string_port('kernel_size', default='3'))
+        self.small_model = ast.literal_eval(self.get_input_string_port('small_model', default='False'))
+
+        # Get input data port, navigate to chip directory
+        self.chip_dir = self.get_input_data_port('chips')
+
+        # Format working directory
+        os.chdir(self.chip_dir) # !!!!! Now in chip directory for remainder of task
+        os.makedirs('models') # Create directory to save model after each epoch
+        ref_geoj = [f for f in os.listdir('.') if f.endswith('ref.geojson')][0]
+        chips = [f for f in os.listdir('.') if f.endswith('.tif')]
+
+        # Get chip and reference geojson info
+        self.geojson = os.path.join(self.chip_dir, ref_geoj)
+        self.chips = [os.path.join(self.chip_dir, ch) for ch in chips]
+        self.n_bands = gdal.Open(self.chips[0]).RasterCount
+        logging.info('Detected number of bands: ' + str(self.n_bands))
+
+        # Get input_shape info
+        if self.resize_dim:
+            self.input_shape = [self.n_bands, self.resize_dim, self.resize_dim]
+        else:
+            self.input_shape = [self.n_bands, self.max_side_dim, self.max_side_dim]
+        logging.info('Input shape: ' + str(self.input_shape))
+
+        # Create output data ports
+        self.out_dir = self.get_output_data_port('trained_model')
+        self.model_weights = os.path.join(self.out_dir, 'model_weights')
+        self.rnd_1 = os.path.join(self.model_weights, 'round_1')
+        self.info_dir = os.path.join(self.out_dir, 'info')
+        os.makedirs(self.out_dir)
+        os.makedirs(self.model_weights)
+        os.makedirs(self.rnd_1)
+        os.makedirs(self.info_dir)
+
+        if self.two_rounds:
+            self.rnd_2 = os.path.join(self.model_weights, 'round_2')
+            os.makedirs(self.rnd_2)
+
+        # Create class reference for one-hot encoded classes
+        self.class_dict = {}
+        for clss_ix in xrange(len(self.classes)):
+            class_array = np.zeros(len(self.classes))
+            class_array[clss_ix] = 1.
+            self.class_dict[self.classes[clss_ix]] = class_array
+
+
+    def filter_chips(self):
+        '''
+        Remove chips that are too large or too small. Remove entries in ref.geojson that
+            are not valid chips.
+        This method should be called from the chips directory, which contains the
+            reference geojson for each chip.
+        '''
+        # Open reference geojson
+        with open(self.geojson) as f:
+            data = geojson.load(f)
+
+        feature_collection = data['features']
+        print str(len(feature_collection)) + ' initial features'
+        valid_feats = []
+
+        # Filter chips
+        for feat in feature_collection:
+            chip_name = str(feat['properties']['feature_id']) + '.tif'
+            chip = gdal.Open(chip_name)
+
+            try:
+                min_side = min(chip.RasterXSize, chip.RasterYSize)
+                max_side = max(chip.RasterXSize, chip.RasterYSize)
+            except (AttributeError):
+                logging.debug('Chip not found in directory: ' + chip_name)
+                continue
+
+            if min_side < self.min_side_dim or max_side > self.max_side_dim:
+                logging.debug('Wrong sized chip: ' + chip_name)
+            else:
+                valid_feats.append(feat)
+
+        data['features'] = valid_feats
+        print str(len(valid_feats)) + ' features'
+
+        # Replace self.geojson with only valid polygons
+        with open('filtered.geojson', 'wb') as f:
+            geojson.dump(data, f)
+
+        return 'filtered.geojson'
+
+
+    def format_geojson(self):
+        '''
+        Prep geojson by balancing (if necessary) and doing a train/test split.
+            returns input geojson filepath.
+        '''
+        train_geoj = self.geojson
+
+        # Create train/test data geojsons
+        if self.test:
+            gt.create_train_test(self.geojson, output_file='input.geojson',
+                                 test_size=self.test_size)
+            train_geoj, test_geoj = 'train_input.geojson', 'test_input.geojson'
+
+        # Create balanced datasets
+        if self.two_rounds:
+            gt.create_balanced_geojson(train_geoj, classes=self.classes,
+                                       output_file='train_balanced.geojson')
+            train_geoj = 'train_balanced.geojson'
+
+        # Confirm adequate amount of training data for train_size
+        with open(train_geoj) as inp_file:
+            poly_ct = len(geojson.load(inp_file)['features'])
+
+        if poly_ct < self.train_size:
+            raise Exception('There are only {} polygons that can be used as training ' \
+                            'data, cannot train the network on {} samples. Please ' \
+                            'decrease train_size or provide more ' \
+                            'polygons.'.format(str(poly_ct), str(self.train_size)))
+
+        # Return name of input training file
+        return train_geoj
+
+
+    def get_chips_from_features(self, feature_collection):
+        '''
+        Load chips into memory from a list of features
+        Each chip will be padded to the input side dimension
+        '''
+        X, y, chip_names = [], [], []
+        norm_val = float(2 ** self.bit_depth - 1)
+
+        # Get chip names for each feature
+        for feat in feature_collection:
+            name = str(feat['properties']['feature_id']) + '.tif'
+            clss = str(feat['properties']['class_name'])
+            chip_names.append([name, clss])
+
+        for chip, clss in chip_names:
+            raster_array = []
+            ds = gdal.Open(chip)
+            bands = ds.RasterCount
+
+            # Create normed raster array
+            for n in xrange(1, bands + 1):
+                raster_array.append(ds.GetRasterBand(n).ReadAsArray() / norm_val)
+
+            # pad to input shape
+            chan, h, w = np.shape(raster_array)
+            pad_h, pad_w = self.max_side_dim - h, self.max_side_dim - w
+            chip_patch = np.pad(raster_array, [(0, 0), (pad_h/2, (pad_h - pad_h/2)),
+                                (pad_w/2, (pad_w - pad_w/2))], 'constant',
+                                constant_values=0)
+
+            # resize chip
+            if self.resize_dim:
+                new_chip = []
+                for band_ix in xrange(len(chip_patch)):
+                    new_chip.append(imresize(chip_patch[band_ix],
+                                    self.input_shape[-2:]).astype(float))
+                chip_patch = np.array(new_chip)
+
+            # Add raster array and one-hot class to X and y
+            X.append(chip_patch)
+            y.append(self.class_dict[clss])
+
+            # sys.stdout.write('\r%{0:.2f}'.format(100 * (len(y)) / float(len(feature_collection))) + ' ' * 20)
+            # sys.stdout.flush()
+
+        return np.array(X), np.array(y)
+
+
+    def train_model(self, model, train_geojson, retrain=False):
+        '''
+        Train model using the chips referenced in train_geojson. Returns a trained
+            model and a history of all validation losses
+        '''
+        validation_data, full_hist = None, []
+        rnd = '2' if retrain else '1'
+
+        with open(train_geojson) as f:
+            feats = geojson.load(f)['features']
+            np.random.shuffle(feats)
+
+        # Freeze layers if retraining model
+        nb_epoch = self.nb_epoch if not retrain else self.nb_epoch_2
+        train_size = self.train_size if not retrain else self.train_size_2
+        if retrain:
+            for i in xrange(len(model.layers[:-1])):
+                model.layers[i].trainable = False
+                sgd = SGD(lr=self.lr_2, momentum=0.9, nesterov=True)
+                model.compile(loss='categorical_crossentropy', optimizer='sgd')
+
+        # Get validation data (10% of training data)
+        val_size = int(0.1 * self.train_size)
+        val_data, train_data = feats[:val_size], feats[val_size: train_size]
+        train_size = len(train_data)
+        valX, valY = self.get_chips_from_features(val_data)
+
+        for e in range(nb_epoch):
+            print 'Epoch {}/{}'.format(e + 1, nb_epoch)
+            chk = ModelCheckpoint(filepath="./models/epoch" + str(e) + \
+                                  "_{val_loss:.2f}.h5", verbose=1)
+            np.random.shuffle(train_data)
+
+            # Cycle through batches of chips and train
+            for batch_start in range(0, train_size, 1000):
+                callbacks = []
+                this_batch = train_data[batch_start: batch_start + 1000]
+
+                # Generate chips
+                X,y = self.get_chips_from_features(this_batch)
+                if batch_start == range(0, train_size, 1000)[-1]:
+                    callbacks = [chk]
+
+                # Fit the model
+                hist = model.fit(X, y, batch_size=self.batch_size, nb_epoch=1,
+                                 validation_data=(valX, valY), callbacks=callbacks)
+
+            full_hist.append(hist.history)
+
+        # Load model weights with the lowest validation loss
+        if self.use_lowest_val_loss and rnd == '1':
+            val_losses = [epoch['val_loss'][0] for epoch in full_hist]
+            min_epoch = np.argmin(val_losses)
+            min_loss = val_losses[np.argmin(val_losses)]
+            min_weights = 'models/epoch' + str(min_epoch) + '_{0:.2f}.h5'.format(min_loss)
+            model.load_weights(min_weights)
+
+        # Save all models to output dir
+        save_path = os.path.join(self.model_weights, 'round_{}'.format(rnd))
+        for weights in os.listdir('models/'):
+            shutil.move('models/' + weights, save_path)
+
+        return model
+
+
+    def test_net(self, model):
+        '''
+        test network performance on test data
+        '''
+        y_pred, y_true, test_report = [], [], ''
+        with open('test_input.geojson') as f:
+            test_data = geojson.load(f)['features']
+
+        for feat_ix in xrange(0, self.test_size, 1000):
+            x, y = self.get_chips_from_features(test_data[feat_ix: feat_ix + 1000])
+
+            y_pred += list(model.predict_classes(x))
+            y_true += [int(np.argwhere(clss == 1)) for clss in y]
+
+        test_size = len(y_true)
+        y_pred, y_true = np.array(y_pred), np.array(y_true)
+
+        test_report = classification_report(y_true, y_pred, target_names=self.classes)
+
+        # Record test results
+        with open(os.path.join(self.out_dir, 'test_report.txt'), 'w') as tr:
+            tr.write(test_report)
+
+
+    def invoke(self):
+        '''
+        Execute task
+        '''
+        # Filter chips and update reference geojson
+        self.geojson = self.filter_chips()
+
+        # Get train, test and balanced geojsons
+        train_geojson = self.format_geojson()
+
+        # Create model architecture
+        p = PoolNet(self.classes, batch_size=self.batch_size,
+                    input_shape=self.input_shape, learning_rate=self.lr_1,
+                    small_model=self.small_model, kernel_size=self.kernel_size)
+        model = p.model
+
+        # Fit model rnd 1
+        mem_error = 'Model does not fit in memory. Please try one or more of the ' \
+                    'following: (1) Use resize_dim to downsample chips. (2) Use a ' \
+                    'smaller batch_size. (3) Set the small_model flag to True.'
+        try:
+            model = self.train_model(model=model, train_geojson=train_geojson)
+        except (MemoryError):
+            raise Exception(mem_error)
+
+        # Training round two
+        if self.two_rounds:
+            try:
+                model = self.train_model(model=model, retrain=True,
+                                         train_geojson='train_input.geojson')
+            except (MemoryError):
+                raise Exception(mem_error)
+
+
+        if self.test:
+            self.test_net(model=model)
+
+        # Save model and classes to output directory
+        model.save(os.path.join(self.out_dir, 'trained_model.h5'))
+
+        classes_json = {'classes': str(self.classes)}
+        with open(os.path.join(self.info_dir, 'classes.json'), 'wb') as f:
+            json.dump(classes_json, f)
+
+        # Save train and test data to info directory of output
+        shutil.copy(train_geojson, os.path.join(self.info_dir, 'train_data.geojson'))
+        shutil.copy('test_input.geojson', os.path.join(self.info_dir, 'test_data.geojson'))
+
+if __name__ == '__main__':
+    with TrainCnnChipClassifier() as task:
+        task.invoke()
---
+++ train-cnn-chip-classifier.md
@@ -0,0 +1,201 @@
+# Train CNN Chip Classifier
+
+A GBDX task that takes GeoTiff image chips created from the [chip-from-vrt](https://github.com/PlatformStories/chip-from-vrt) task and produces a trained Convolutional Neural Network (CNN) classifier. The network architecture is [VGG Net](https://arxiv.org/pdf/1409.1556.pdf), which was developed as part of the [2014 ImageNet](http://www.image-net.org/challenges/LSVRC/2014/) challenge.
+
+<img src='images/train-cnn-chip-classifier.png' width=400>
+
+
+## Run
+
+Here we execute an example in which a classifier is trained to find buildings in Nigeria from image chips stored on S3. Note that there is also a geojson 'ref.geojson' in this location with class names ('No Buildings' or 'Buildings') for each chip, which will be used to train the network. The chips and reference geojson are all outputs of the [chip-from-vrt](https://github.com/PlatformStories/chip-from-vrt) task.
+
+1. In a Python terminal create a GBDX interface and specify the task input location:
+
+    ```python
+    from gbdxtools import Interface
+    from os.path import join
+    import random
+    import string
+
+    gbdx = Interface()
+
+    input_location = 's3://gbd-customer-data/58600248-2927-4523-b44b-5fec3d278c09/platform-stories/train-cnn-chip-classifier/'
+    ```
+
+2. Create a task instance and set the required inputs:  
+
+    ```python
+    train_task = gbdx.Task('train-cnn-chip-classifier')
+    train_task.inputs.chips = join(input_location, 'chips')
+    train_task.inputs.classes = 'No Buildings, Buildings'
+    train_task.inputs.max_side_dim = '270'
+    train_task.inputs.resize_dim = '150'
+    ```
+
+3. Set any optional hyper-parameters if necessary. With the following parameters training should take about four hours to complete:
+
+    ```python
+    train_task.inputs.two_rounds = 'False'
+    train_task.inputs.nb_epoch = '50'
+    train_task.inputs.train_size = '2500'
+    train_task.inputs.test_size = '2000'
+    train_task.inputs.bit_depth = '8'
+    ```
+
+4. Initialize a workflow and specify where to save the output:
+
+    ```python
+    train_wf = gbdx.Workflow([train_task])
+    random_str = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
+    output_location = join('platform-stories/trial-runs', random_str)
+
+    train_wf.savedata(train_task.outputs.trained_model, join(output_location, 'trained_model'))
+    ```
+
+5. Execute the workflow:  
+
+    ```python
+    train_wf.execute()
+    ```
+
+6. Track the status of the workflow as follows:
+
+    ```python
+    train_wf.status
+    ```
+
+
+## Input Ports  
+
+The task input ports. Note that booleans, integers and floats **must be** passed to the task as strings, e.g., 'True', '10', '0.001'.
+
+| Name  | Type | Description:                                      | Required |
+|-------|---------|--------------|---------------------------------------------------|
+| chips | String | Contains GeoTiff image chips (feature_id.tif) produced from chips-s3-imagery. Should also contain a geojson 'ref.geojson' with class names for each chip (referenced by feature id). | True |
+| classes  | String | Classes to train network on, each separated by a comma (e.g- 'No Buildings, Buildings'). Must be exactly as they appear in the class_name property of the geojson used to create the chips in the chip-from-vrt task. | True |
+| [min_side_dim](#side-dimensions) | Int | The minimum acceptable side dimension (in pixels) for training chips. Defaults to 0. | False |
+| [max_side_dim](#side-dimensions)  | Int | The maximum acceptable side dimension (in pixels) for training chips. Defaults to 150. | False |
+| [resize_dim](#resize_dim) | Int | Dimension to resize the chip side dimensions to. This should be smaller than the original dimension. Defaults to None | False |
+| [two_rounds](#two_rounds) | Bool | If True, train the network in two rounds- first on balanced classes then on the original distribution of classes. In the second training round only the weights of the final layer of the model will be updated. Recommended if there is class imbalance in the dataset. Defaults to False | False |
+| [train_size](#training-size) | Int | Number of chips to train the network on for the first round of training. Defaults to 10000. | False |
+| [train_size_2](#training-size)  | Int | Number of chips to train on in the second round of training. Only relevant if two_rounds is True. Defaults to half of train_size | False |
+| [batch_size](#batch-size)  | Int | Number of chips to train on per batch. Defaults to 32 | False |
+| [nb_epoch](#nb_epoch) | Int | Number of training epochs to perform for the first round of training. Defaults to 35 | False |
+| [nb_epoch_2](#nb_epoch) | Int | Number of training epochs to perform for the second round of training. Only relevant if two_rounds is True. Defatults to 8. | False |
+| [use_lowest_val_loss](#use_lowest_val_loss)  | Bool | After the first round of training use the model weights that yielded the lowest validation loss as the final model (recommended). Otherwise the model weights after the final epoch will be used. Defaults to True | False |
+| [test](#testing)  | Bool | If True testing will be completed on a subset of chips. A test report with accuracy metrics will be saved as a text file in the 'model' output directory. Defaults to True. | False |
+| [test_size](#testing)  | Int | Number of chips to test on. Only relevant if test is True. Defaults to 1000. | False |
+| [learning_rate](#learning-rate)  | Float | Learning rate for the first round of training. Defaults to 0.001 | False |
+| [learning_rate_2](#learning-rate)  | Float | Learning rate for the second round of training (if applicable). Defaults to 0.01. | False |
+| [bit_depth](#bit_depth)  | Int | Bit depth of the image strips in images. This parameter is necessary for proper normalization. Defaults to 8. | False |
+| [kernel_size](#kernel_size) | Int | Side dimension (in pixels) of the kernels at each convolutional layer in the network. Defaults to 3. | False |
+| small model | Bool | Use a model with 8 layers instead of 16. Useful for large input images (>250 pixels). Defaults to False. | False |
+
+
+## Output Ports  
+
+train-cnn-chip-classifier has one output directory port, trained_model, which is described below:
+
+| Name  | Type | Description:                                      |
+|-------|------|---------------------------------------------------|
+| trained_model | Directory | The fully trained model saved as an h5 file. Models after all epochs will be saved in a subdirectory called model_weights, with the format epoch{n}_{validation_loss}.h5, where n is the epoch that produced the weights. This directory will also contain a test report, which will only have accuracy metrics if train is True. |
+
+## Advanced  
+
+This section contains additional information that provides further insight into training parameters and suggestions for training an effective model.
+
+
+### two_rounds  
+
+The two_rounds flag is a method for dealing with data that has natural class imbalance (unequal representation of image classes). Training a CNN on data with unbalanced classes often results in the network classifying all target data under the majority class. two_rounds avoids this by training in the following steps:
+1. Train the network on balanced data (the task will take care of creating a balanced training dataset).  
+2. Retrain the network on the original class distribution to account for the probability of encountering a given image class. This round will only update the weights to the output layer of the network.
+
+    <img src='images/training_scheme.png' width=400>
+
+This two-round training process allows the network to learn to distinguish between classes based on distinct features (round one) and then learn the probability of encountering each class (round two). This is highly recommended for data that is not balanced.
+
+### side dimensions
+
+Constraints on the minimum and maximum size (in pixels) of a chip. Side dimensions are based on the size of chips bounding box (white line below).
+
+<img src='images/chip_dims.png' width=250>
+
+A sample input chip is displayed above. Note that CNNs require all train/test inputs to have identical dimensions. Thus, all chips are zero-padded to the following dimensions: (num_bands, max_side_dim, max_side_dim). If resize_dim is set, the chips will then be resized to (num_bands, resize_dim, resize_dim). This means that any chips that have a side dimension larger than max_side_dim will not be used during training.
+
+### training size
+
+Number of chips to train on. If not enough chips are provided the task will throw an error.
+
+Notice that if training takes place in [two_rounds](#two-rounds), the maximum train size will be as follows: size of smallest class * number of classes, assuming that all chips are of valid size (as defined by min_side_dim and max_side_dim). Additionally if [testing](#testing) is performed the test data will be subtracted from the available training chips.
+
+### batch size
+
+Number of chips to train on per batch. The model weights will be updated following each batch. Smaller batch sizes can help avoid local minima by increasing the amount of noise in the gradient.
+
+### nb_epoch
+
+Number of training epochs to complete. The validation loss tends to decrease with each successive epoch until a minimum loss is reached. At this point any additional training epochs may result in overfitting.
+
+### use_lowest_val_loss
+
+While the validation loss of the model tends to decrease with successive training epochs, it rarely does so monotonically. Furthermore, if too many training epochs are completed the model may begin to overfit, causing the validation loss to increase with successive epochs. The loss may therefore not be at a minimum at the end of training. Use this flag to ensure that the initial round returns a model with the lowest possible validation loss.
+
+Note that all model weights will be returned in the model_weights folder of the output directory.
+
+### testing
+
+Testing should only be done on two-class classifications. The following explanations assume the classes are input to the task as follows: 'Negative class, Positive class'.
+
+If the test flag is set to True, the task will put aside a set of chips to get accuracy metrics for the trained model. Set this to False if you would like to complete testing manually.
+
+The following metrics are provided in test results:
+* Precision: True Positives / (True Positives + False Positives)
+* Recall: True Positives / (True Positives + False Negatives)
+* F1 score: (2 * Precision * Recall) / (Precision + Recall)
+
+### kernel_size
+
+Each convolutional layer of a CNN uses kernels to extract features from the input image to create an output feature map that is passed to the next layer. This parameter specifies the side dimension of these kernels to use to train the network. Note that increasing the kernel size will slow down training dramatically. Finding the ideal kernel size for a specific use case is often a matter of trial and error.
+
+### resize_dim
+
+There may be memory errors when the input chips are too large (over 250px). This argument will downsample the side dimension of input chips to the specified number of pixels.
+
+
+## Development
+
+### Build the Docker Image
+
+You need to install [Docker](https://docs.docker.com/engine/installation/).
+
+Clone the repository:
+
+```bash
+git clone https://github.com/platformstories/train-cnn-chip-classifier
+```
+
+Then:
+
+```bash
+cd train-cnn-chip-classifier
+docker build -t yourusername/train-cnn-chip-classifier .
+```
+
+Then push the image to Docker Hub:
+
+```bash
+docker push yourusername/train-cnn-chip-classifier
+```
+
+The image name should be the same as the image name under containerDescriptors in train-cnn-chip-classifier.json.
+
+
+### Register on GBDX
+
+In a Python terminal:
+
+```python
+from gbdxtools import Interface
+gbdx = Interface()
+gbdx.task_registry.register(json_filename='train-cnn-chip-classifier.json')
